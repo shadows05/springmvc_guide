826079a668f131b9938c9e54ff45a6b4c11a59e8
unknown <gaoduntao@1a6p-D9994050.lefeng.max>
2015-12-01 15:37:52 +0800

    interceptors

---
+++ src/main/java/com/packt/webstore/interceptor/PerformanceMonitorInterceptor.java
@@ -0,0 +1,66 @@
+package com.packt.webstore.interceptor;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.util.StopWatch;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class PerformanceMonitorInterceptor implements HandlerInterceptor {
+
+	ThreadLocal<StopWatch> stopWatchLocal = new ThreadLocal<StopWatch>();
+	Logger logger = Logger.getLogger(this.getClass());
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception exception)
+			throws Exception {
+		// TODO Auto-generated method stub
+		StopWatch stopWatch = stopWatchLocal.get();
+		stopWatch.stop();
+		logger.info("Total time taken for processing: "
+				+ stopWatch.getTotalTimeMillis() + " ms");
+		stopWatchLocal.set(null);
+		logger.info("=======================================================");
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2, ModelAndView arg3) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("Request processing ended on " + getCurrentTime());
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		// TODO Auto-generated method stub
+		StopWatch stopWatch = new StopWatch(handler.toString());
+		stopWatch.start(handler.toString());
+		stopWatchLocal.set(stopWatch);
+		logger.info("Accessing URL path: " + getURLPath(request));
+		logger.info("Request processing started on: " + getCurrentTime());
+		return true;
+	}
+
+	private String getURLPath(HttpServletRequest request) {
+		String currentPath = request.getRequestURI();
+		String queryString = request.getQueryString();
+		queryString = queryString == null ? "" : "?" + queryString;
+		return currentPath + queryString;
+	}
+
+	private String getCurrentTime() {
+		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy 'at' hh:mm:ss");
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		return formatter.format(calendar.getTime());
+	}
+
+}
